0fbf18f6c0963af2eb1bf73ad2106c9d7cf3f7c8
James Halliday <mail@substack.net>
2013-07-20 20:51:51 -0700

    using a core transform stream

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-var through = require('through');
+var Transform = require('readable-stream/transform');
 
 module.exports = function (time) {
     var to = null;
     var buffer = [];
     var ended = false;
     
-    return through(write, end);
-    
-    function write (buf) {
+    var tr = new Transform;
+    tr._transform = function (buf, enc, next) {
         var tr = this;
         if (typeof buf === 'string') buffer.push(Buffer(buf))
         else buffer.push(buf)
         
         if (!to) to = setTimeout(function () {
-            tr.queue(Buffer.concat(buffer));
+            tr.push(Buffer.concat(buffer));
             buffer = [];
-            if (ended) tr.queue(null);
+            if (ended) tr.push(null);
             to = null;
         }, time);
-    }
+        
+        next();
+    };
     
-    function end () {
+    tr._flush = function () {
         ended = true;
-        if (!to) this.queue(null)
-    }
+        if (!to) tr.push(null)
+    };
+    
+    return tr;
 };
