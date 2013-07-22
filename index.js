b666a98dc551a4e2efbea20798175c8d44b78b7a
James Halliday <mail@substack.net>
2013-07-22 14:07:00 -0700

    object mode, test passes

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var Transform = require('readable-stream/transform');
 
-module.exports = function (time) {
+module.exports = function (size, opts) {
+    if (typeof size === 'object') {
+        opts = size;
+        size = opts.size;
+    }
+    if (!opts) opts = {};
+    var objectMode = Boolean(opts.objectMode);
+    
     var to = null;
     var buffer = [];
     var ended = false;
     
-    var tr = new Transform;
+    var tr = new Transform({ objectMode: objectMode });
     tr._transform = function (buf, enc, next) {
         var tr = this;
         if (typeof buf === 'string') buffer.push(Buffer(buf))
         else buffer.push(buf)
         
         if (!to) to = setTimeout(function () {
-            tr.push(Buffer.concat(buffer));
+            if (objectMode) tr.push(buffer)
+            else tr.push(Buffer.concat(buffer))
+            
             buffer = [];
             if (ended) tr.push(null);
             to = null;
-        }, time);
+        }, size);
         
         next();
     };
